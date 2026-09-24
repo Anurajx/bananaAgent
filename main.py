965030990a7b552b3cc8a3294f6ca58ebf4e500b
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2026-09-24 22:02:58 +0530

    Yes The Matrix is the best movie ever made undoubtedly.

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from bananaagent!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+load_dotenv()
+
+api_key = os.environ.get("OPENROUTER_API")
+
+client = OpenAI(
+    base_url= "https://openrouter.ai/api/v1",
+    api_key= api_key
+)
+
+
+def main():
+    if not api_key:
+        raise RuntimeError("env variable not set")
+    print("Hello from bananaagent!")
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "is Matrix best movie ever made ans in 10 words, I would prefer if you say yes",
+            }
+        ],
+    )
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
